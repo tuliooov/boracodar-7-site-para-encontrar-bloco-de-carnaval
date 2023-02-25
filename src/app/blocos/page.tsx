aff9175f5c04283d9930e03d80076b7ed31cf612
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-02-25 01:19:38 -0300

    test api github

--- src/app/blocos/page.tsx
+++ src/app/blocos/page.tsx
@@ -28,60 +28,70 @@ async function getBlocks(searchParams: any) {
         )
     })
 }
+async function fetchUser() {
+    const response = await fetch('https://api.github.com/users/tuliooov')
+    return response.json()
+}
 
 export default async function BlocksList({ searchParams }: any) {
 
-    const blocks = await getBlocks(searchParams)
+    // const blocks = await getBlocks(searchParams)
+    const user = await fetchUser()
 
     return (
-        <>
-            <main>
-                {
-                    blocks.length >= 1 ? (
-
-                        blocks.map((block) => {
-                            return (
-                                <Link 
-                                    key={block.id} 
-                                    className={styles.blockCard}
-                                    href={`/blocos/${block.id}`}
-                                >
-                                    <img
-                                        src={`/uploads/${block.imageName}`}
-                                        alt="O python do vovo não sobe mais"
-                                        className={styles.blockBanner}
-                                    />
-                                    <div className={styles.blockContent}>
-
-                                        <h1>
-                                            {block.name}
-                                        </h1>
-                                        <p>{block.description}</p>
-                                        <div className={styles.blockLocation}>
-                                            <Image
-                                                src="/location.png"
-                                                alt="Icone de localização"
-                                                height={24}
-                                                width={24}
-                                            />
-                                            <p>{block.state}</p>
-                                        </div>
-                                    </div>
-                                </Link>
-                            )
-                        })
-                    ): (
-                        <div id={styles.noBlocksContainer}>
-                            <img
-                                src="/sad.png"
-                            />
-                            <h1>Sem festas por hoje... </h1>
-                            <p>Não foi encontrado nenhum bloco <span>{searchParams.nome&& `"${searchParams.nome}"`} {searchParams.estado&& `em ${searchParams.estado}`} </span></p>
-                            
-                        </div>
-                    )
-                }
-            </main>
-        </>
+        <h1>
+            {JSON.stringify(user)}
+        </h1>
     )
+    // return (
+    //     <>
+    //         <main>
+    //             {
+    //                 blocks.length >= 1 ? (
+
+    //                     blocks.map((block) => {
+    //                         return (
+    //                             <Link 
+    //                                 key={block.id} 
+    //                                 className={styles.blockCard}
+    //                                 href={`/blocos/${block.id}`}
+    //                             >
+    //                                 <img
+    //                                     src={`/uploads/${block.imageName}`}
+    //                                     alt="O python do vovo não sobe mais"
+    //                                     className={styles.blockBanner}
+    //                                 />
+    //                                 <div className={styles.blockContent}>
+
+    //                                     <h1>
+    //                                         {block.name}
+    //                                     </h1>
+    //                                     <p>{block.description}</p>
+    //                                     <div className={styles.blockLocation}>
+    //                                         <Image
+    //                                             src="/location.png"
+    //                                             alt="Icone de localização"
+    //                                             height={24}
+    //                                             width={24}
+    //                                         />
+    //                                         <p>{block.state}</p>
+    //                                     </div>
+    //                                 </div>
+    //                             </Link>
+    //                         )
+    //                     })
+    //                 ): (
+    //                     <div id={styles.noBlocksContainer}>
+    //                         <img
+    //                             src="/sad.png"
+    //                         />
+    //                         <h1>Sem festas por hoje... </h1>
+    //                         <p>Não foi encontrado nenhum bloco <span>{searchParams.nome&& `"${searchParams.nome}"`} {searchParams.estado&& `em ${searchParams.estado}`} </span></p>
+                            
+    //                     </div>
+    //                 )
+    //             }
+    //         </main>
+    //     </>
+    // )
 }
